1102b763cfed8b4e5c87720e099ece68ed793400
vishalsinghhh <vishalsingh0710vs@gmail.com>
2021-09-27 19:48:11 +0530

    Saving data

--- cards.js
+++ cards.js
@@ -1,4 +1,5 @@
 const cafeList = document.querySelector("#birthday-list")
+const form = document.querySelector("#add-birthday-form");
 
 function renderCafe(doc){
   let li=document.createElement('li');
@@ -15,8 +16,21 @@ function renderCafe(doc){
   cafeList.appendChild(li);
 }
 
+// GETTING DATA 
 db.collection('Birthdays').get().then((snapshot) => {
   snapshot.docs.forEach(doc=>{
     renderCafe(doc);
   })
+})
+
+// SAVING DATA 
+form.addEventListener('submit', (e)=>{
+  e.preventDefault();
+  db.collection("Birthdays").add({
+    FirstName: form.firstName.value,
+    LastName: form.lastName.value,
+    Email: form.email.value,
+    DOB: form.dob.value,
+    PhoneNo: form.phone.value,
+  });
 })
