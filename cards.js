b4ec2ab31a771d1b3ebd7cb752a76ab55741f751
vishalsinghhh <vishalsingh0710vs@gmail.com>
2021-09-27 19:22:23 +0530

    Getting Documents

--- cards.js
+++ cards.js
@@ -1,3 +1,22 @@
+const cafeList = document.querySelector("#birthday-list")
+
+function renderCafe(doc){
+  let li=document.createElement('li');
+  let firstName = document.createElement('span')
+  let lastName = document.createElement('span')
+
+  li.setAttribute('data-id', doc.id);
+  firstName.textContent = doc.data().FirstName;
+  lastName.textContent = doc.data().LastName;
+
+  li.appendChild(firstName);
+  li.appendChild(lastName);
+
+  cafeList.appendChild(li);
+}
+
 db.collection('Birthdays').get().then((snapshot) => {
-  console.log(snapshot.docs)
+  snapshot.docs.forEach(doc=>{
+    renderCafe(doc);
+  })
 })
